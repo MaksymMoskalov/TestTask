d7281d1f04dda64a18ca1d84770a62c0eb0153f6
MaksymMoskalov <128045448+MaksymMoskalov@users.noreply.github.com>
2024-02-16 00:52:17 +0000

    Deploying to gh-pages from @ MaksymMoskalov/TestTask@9e8609dfe67793d98d2f3a2498032f488fda8768 🚀

---
+++ static/js/395.c9efb54c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkTestTask=self.webpackChunkTestTask||[]).push([[395],{7395:function(s,e,t){t.r(e);e.default=function(){}}}]);
+//# sourceMappingURL=395.c9efb54c.chunk.js.map
